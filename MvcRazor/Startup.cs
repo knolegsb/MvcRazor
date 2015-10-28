ca4fc5c67e713d9477c278538d3d87f179853610
knolegsb <seon2you@hotmail.com>
2015-10-27 22:51:45 -0700

    start and added model

---
+++ MvcRazor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcRazor.Startup))]
+namespace MvcRazor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
